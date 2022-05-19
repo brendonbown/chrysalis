e8334dbc76e562b51af7ce92d26bff23bf00a265
Brendon Bown <bhb49@byu.edu>
2022-05-18 18:39:54 -0600

    Began work on args

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
     implementation("com.oracle.database.jdbc:ojdbc8-production:21.5.0.0")
+    implementation("com.xenomachina:kotlin-argparser:2.0.7")
 }
 
 tasks.test {
