fcfa40ebf68a681ccb6832676a1926da3d5acf05
Brendon Bown <bhb49@byu.edu>
2022-05-27 18:35:52 -0600

    Refactored code

--- src/main/kotlin/Common.kt
+++ src/main/kotlin/Common.kt
@@ -1,3 +1,11 @@
 fun printError(error: String) {
     System.err.println("ERROR: $error")
 }
+
+fun prompt(promptText: String, hideInput: Boolean = false): String? {
+    print(promptText)
+    return if (hideInput)
+        System.console()?.readPassword()?.toString() ?: readLine()
+    else
+        readLine()
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -72,11 +72,3 @@ fun main(args: Array<String>) {
         }
     }
 }
-
-fun prompt(promptText: String, hideInput: Boolean = false): String? {
-    print(promptText)
-    return if (hideInput)
-        System.console()?.readPassword()?.toString() ?: readLine()
-    else
-        readLine()
-}
