4b7c1d42b027ca1146321af6e965bcd8159b0c27
Brendon Bown <bhb49@byu.edu>
2023-04-12 17:03:05 -0600

    Extract model into model package

---
+++ src/main/kotlin/model/Action.kt
@@ -0,0 +1,21 @@
+package model
+
+import com.xenomachina.argparser.InvalidArgumentException
+
+enum class Action {
+    LIST, // List all permissions for the user
+    ADD, // Add a permission/set of permissions for the user
+    COPY, // Copy permissions from a given person
+    REMOVE, // Add a permission/set of permissions for the user
+    VERSION; // Get version information
+
+    override fun toString(): String {
+        return when (this) {
+            LIST -> "list"
+            ADD -> "add"
+            COPY -> "copy"
+            REMOVE -> "remove"
+            VERSION -> "version"
+        }
+    }
+}
---
+++ src/main/kotlin/model/Identifier.kt
@@ -0,0 +1,17 @@
+package model
+
+// A sum type representing the different kinds of identifiers
+sealed class Identifier {
+
+    class PersonId(val personId: String) : Identifier() {
+        override fun toString() = "Person ID<$personId>"
+    }
+
+    class ByuId(val byuId: String) : Identifier() {
+        override fun toString() = "BYU ID<$byuId>"
+    }
+
+    class NetId(val netId: String) : Identifier() {
+        override fun toString() = "Net ID<$netId>"
+    }
+}
